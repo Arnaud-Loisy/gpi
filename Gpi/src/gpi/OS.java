2c97edc421a17518ab0fcc9c3bb1804c8736fc5d
darkey <loisy.arnaud@gmail.com>
2013-05-15 15:27:58 +0200

    classe OS

---
+++ Gpi/src/gpi/OS.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gpi;
+
+/**
+ *
+ * @author Arnaud Loisy
+ */
+public class OS {
+    
+    private String Nom;
+    private String Version;
+}
