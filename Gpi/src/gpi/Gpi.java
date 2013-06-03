98db7ca7d442410887e958e825b5c9b2528f1b24
Arnaud <loisy.arnaud@gmail.com>
2013-06-03 07:37:40 +0200

    Add entrepot

--- Gpi/src/gpi/Gpi.java
+++ Gpi/src/gpi/Gpi.java
@@ -20,6 +20,12 @@ public class Gpi {
 	public static void main(String[] args) {
 		// TODO code application logic here
 		ParcInfo parcInfo = new ParcInfo();
+                
+                Batiment entrepot = new Batiment("Entrepot");
+                Salle stock = new Salle("Stock");
+                
+                parcInfo.ajouterNouveauBatiment(entrepot);
+                parcInfo.ajouterNouvelleSalle(entrepot, stock);
 /*
 		Batiment batiment0 = new Batiment("Entrepot");
 		Batiment batiment1 = new Batiment("U4");
